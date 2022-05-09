ae75b551e621383e5f70c13a0d06edfce7c79a58
Shabana Kauser <shabanakauser@Shabanas-MacBook-Air.local>
2022-05-09 21:44:44 +0100

    task1 complete

--- task-1/main.js
+++ task-1/main.js
@@ -10,3 +10,55 @@ const celebs = [
   "Richard Osman",
   "Emma Thompson"
 ];
+
+//Make function called 'makeLedgend'
+//Function will take in a 'name' (string) -as only argument
+//Function will return new string which includes 'name' plus the words 'is now a ledged'
+//Use a for loop
+//Create new array which includes 'is now a ledgend' for each item 
+//Save array to the var 'ledgendary celebs'
+//New for loop for new array of all celebrity names which start witha vowel
+//Store new array in new var 'vowelCelebs'
+
+
+//Task 1
+
+let myName = 'Shabana'
+
+function makeLedgend(){
+
+}
+
+console.log(myName + " is now a ledgend")
+
+
+//Task 2
+
+function makeLedgend(name){
+
+for (let i=0; i<celebs.length; i++) {
+  let ledgendaryCelebs = [];
+  ledgendaryCelebs.push(celebs[i]+ " is now a ledgend")
+  console.log(ledgendaryCelebs)
+
+}
+}
+makeLedgend();
+
+//Task 3
+
+function listVowelCelebs(){
+
+for (let i=0; i<celebs.length; i++){
+
+}
+
+}
+
+let vowelCelebs = celebs.filter(str => /^[aeiou]/i.test(str));
+
+console.log(vowelCelebs);
+
+listVowelCelebs();
+
+
--- task-2/main.js
+++ task-2/main.js
@@ -1,1 +1,8 @@
+// let secCount = document.querySelector("#count");
+// let timerFunct = setInterval(function() {secCount.innerHTML++}, 1000);
+// setTimeout(function() {clearInterval(timerFunct);}, 12000);
 
+
+// let theHeading = document.querySelector{"#H1"}
+
+let 
